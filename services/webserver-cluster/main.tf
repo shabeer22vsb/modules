9b39655f9b40c9fb0df3f5e88c3680c849105548
shabeer22vsb <shabeer22vsb@gmail.com>
2026-06-06 16:56:06 +0100

    updated modules with changes

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -214,30 +214,6 @@ resource "aws_security_group" "lb_sg" {
 
   description = "example"
 
-  ingress {
-
-    from_port = 80
-
-    to_port = 80
-
-    protocol = "tcp"
-
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  egress{
-
-    from_port = 0
-
-    to_port = 0
-
-    protocol = "-1"
-
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
 }
 resource "aws_security_group_rule" "lb_sg_ingress" {
   description = "this is the ingress rules for loadbalancers"
