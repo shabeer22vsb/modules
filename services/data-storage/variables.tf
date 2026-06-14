b6974851f7e1f5bad3dc9cc14b6da3e355316a1a
shabeer22vsb <shabeer22vsb@gmail.com>
2026-06-14 18:13:50 +0100

    updated values

--- services/data-storage/variables.tf
+++ services/data-storage/variables.tf
@@ -2,11 +2,13 @@ variable "db_username" {
     description = "Username to login DB"
     type = string
     sensitive = true
+    default     = null
 }
 variable "db_password" {
     description = "password to login DB"
     type = string
     sensitive = true
+    default     = null
 }
 variable "db_instance_class"{
     description = "DB instance type"
